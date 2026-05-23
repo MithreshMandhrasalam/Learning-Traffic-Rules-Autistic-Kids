a1eb50dc055ee1a134b614bff8c2bc0bdf46d5d4
Mithresh <mithreshmandhrasalam59@gmail.com>
2026-05-23 11:52:35 +0530

    Rename app to SafeSteps Kids

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="nav-brand">
         <span className="nav-logo">🚦</span>
-        <span className="nav-title">RoadSafe</span>
+        <span className="nav-title">SafeSteps Kids</span>
       </div>
 
       <div className="nav-links">
